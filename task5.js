a8ef9ac9ce886aa01257508cdd0c11167df8d590
fhhdjfhedjdkjd123 <bash>
2022-12-22 18:23:17 +0530

    updated index, task5

---
+++ task5.js
@@ -0,0 +1,14 @@
+//BYCLASSNAME
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold";
+// }
+
+//GET ELEMENTS BY TAG NAME
+let listItems = document.getElementsByTagName("li");
+console.log(items);
+for(let i=0;i<listItems.length;i++){
+    listItems[i].style.fontWeight="bold";
+}
