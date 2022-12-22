4c882cf9c1768ab0fde8e6f8b3718e45ded08157
fhhdjfhedjdkjd123 <bash>
2022-12-22 17:23:25 +0530

    updated dom,index

---
+++ dom.js
@@ -0,0 +1,24 @@
+// console.dir(document);
+// console.log(document);
+// console.log(document.images);
+// var title = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// var title = document.getElementById("header-title");
+
+// console.log(title);
+// //title.textContent = "Welcome";
+// title.innerText = "Welcome to Dom";
+// console.log(title);
+// header.style.borderBottom = "solid 4px #000";
+//BYCLASSNAME
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+
+item3.style.backgroundColor="green";
+item3.style.fontWeight="bold";
+item1.style.fontWeight="bold";
+item2.style.fontWeight="bold";
+item4.style.fontWeight="bold";
+
